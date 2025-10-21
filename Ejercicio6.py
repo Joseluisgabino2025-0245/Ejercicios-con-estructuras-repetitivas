3fda249b693b396983c78cec7617fff9627b476e
Jose Luis Gabino 2025-0245 <2025-0245@unad.edu.do>
2025-10-21 15:27:56 -0400

    Ejercicio6.py

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -3,4 +3,4 @@
 numero = int(input("Ingresa un número para ver su tabla de multiplicar: "))
 for i in range(1, 11):
     resultado = numero * i
-    print(f"{numero} x {i} = {resultado}")
+    print(f"{numero} x {i} = {resultado}") 
